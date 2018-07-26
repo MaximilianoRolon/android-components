0a3954d11ed9d64314233d31c2b38f5bd4f18d15
Maximiliano Rolon <maxirolon@gmail.com>
2018-07-26 17:53:24 -0300

    Final Version

--- app/src/main/java/com/example/formapp/ConfirmarDatos.java
+++ app/src/main/java/com/example/formapp/ConfirmarDatos.java
@@ -1,13 +1,61 @@
 package com.example.formapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConfirmarDatos extends AppCompatActivity {
+
+    Bundle datos;
+    String fecha;
+    String nombre;
+    String telefono;
+    String email;
+    String descripcion;
+    TextView tvFechaEdit;
+    TextView tvNombreEdit;
+    TextView tvTelefonoEdit;
+    TextView tvEmailEdit;
+    TextView tvDescripcionContactoEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmar_datos);
+
+        datos = this.getIntent().getExtras();
+
+        // EXTRA DE INTENTS
+        fecha = datos.getString("FECHA");
+        nombre = datos.getString("NOMBRE");
+        telefono = datos.getString("TELEFONO");
+        email = datos.getString("EMAIL");
+        descripcion = datos.getString("DESCRIPCION");
+
+        // COMPONENTES Y SETEADO DE LOS MISMOS
+        tvFechaEdit = findViewById(R.id.tvFechaEdit);
+        tvFechaEdit.setText(fecha);
+        tvTelefonoEdit = findViewById(R.id.tvTelefonoEdit);
+        tvTelefonoEdit.setText(telefono);
+        tvEmailEdit = findViewById(R.id.tvEmailEdit);
+        tvEmailEdit.setText(email);
+        tvDescripcionContactoEdit = findViewById(R.id.tvDescripcionContactoEdit);
+        tvDescripcionContactoEdit.setText(descripcion);
+        tvNombreEdit = findViewById(R.id.tvNombreEdit);
+        tvNombreEdit.setText(nombre);
     }
+
+    public void clickEditData(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("FECHA", tvFechaEdit.getText().toString());
+        intent.putExtra("NOMBRE", tvNombreEdit.getText().toString());
+        intent.putExtra("TELEFONO", tvTelefonoEdit.getText().toString());
+        intent.putExtra("EMAIL", tvEmailEdit.getText().toString());
+        intent.putExtra("DESCRIPCION", tvDescripcionContactoEdit.getText().toString());
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/formapp/MainActivity.java
+++ app/src/main/java/com/example/formapp/MainActivity.java
@@ -9,17 +9,51 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvFecha;
+    Bundle datos;
+    EditText tvFecha;
+    EditText etNombre;
+    EditText etTelefono;
+    EditText etEmail;
+    EditText etDescripcionContacto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvFecha = findViewById(R.id.tvFecha);
+        etTelefono = findViewById(R.id.etTelefono);
+        etEmail = findViewById(R.id.etEmail);
+        etDescripcionContacto = findViewById(R.id.etDescripcionContacto);
+        etNombre = findViewById(R.id.etNombre);
+
+        // SETEO INICIAL OPCIONAL DE DATOS
+        Intent intent = this.getIntent();
+
+        if (intent.getExtras() != null){
+            Bundle datos = intent.getExtras();
+            if (datos.getString("FECHA") != null ){
+                tvFecha.setText(datos.getString("FECHA"));
+            }
+            if (datos.getString("TELEFONO") != null ){
+                etTelefono.setText(datos.getString("TELEFONO"));
+            }
+            if (datos.getString("EMAIL") != null ){
+                etEmail.setText(datos.getString("EMAIL"));
+            }
+            if (datos.getString("DESCRIPCION") != null ){
+                etDescripcionContacto.setText(datos.getString("DESCRIPCION"));
+            }
+            if (datos.getString("NOMBRE") != null ){
+                etNombre.setText(datos.getString("NOMBRE"));
+            }
+        }
+
+
     }
 
     public void showDatePickerDialog(View v) {
@@ -29,10 +63,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendData(View view) {
-        Intent intent = new Intent(this, ConfirmarDatos.class);;
-        intent.putExtra("Name", "maxi");
-        intent.putExtra("Name", "maxi");
-        intent.putExtra("Name", "maxi");
+        Intent intent = new Intent(this, ConfirmarDatos.class);
+        intent.putExtra("FECHA", tvFecha.getText().toString());
+        intent.putExtra("NOMBRE", etNombre.getText().toString());
+        intent.putExtra("TELEFONO", etTelefono.getText().toString());
+        intent.putExtra("EMAIL", etEmail.getText().toString());
+        intent.putExtra("DESCRIPCION", etDescripcionContacto.getText().toString());
         startActivity(intent);
     }
 }
